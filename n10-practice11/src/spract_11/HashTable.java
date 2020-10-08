62d73b95eba2f308c86bf3879c1a31f75e85ebd7
Mike-Like11 <mike7677877@gmail.com>
2020-10-08 22:43:53 +0300

    3 доделанная

--- n10-practice11/src/spract_11/HashTable.java
+++ n10-practice11/src/spract_11/HashTable.java
@@ -1,31 +1,72 @@
 package spract_11;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class HashTable<V,E> {
-    int size=1000;
-    ArrayList<E>[]a=new ArrayList[1000];
-    int hash( V value){
+    int size=10;
+   ArrayList<LinkedList<E>>a;
+
+    public HashTable(int size) {
+        this.size = size;
+        this.a=new ArrayList<LinkedList<E>>(this.size);
+        for(int i=0;i<10;i++){
+            a.add(new LinkedList<E>());
+        }
+
+    }
+
+    int hash(int value){
         return (int)value%size;
     }
-    void add( V value, E b) {
-        if(a[hash(value)]==null) {
-            a[hash(value)]=new ArrayList<E>();
+    int hash(E n) {
+        Polic c= (Polic) n;
+        return c.inn % size;
+    }
+   void add(  E b) {
+       a.get(hash(b)).addLast(b);
+       if (a.get(hash(b)).size() > 3) rehash();
+    }
+    void rehash(){
+       ArrayDeque<E>t=new ArrayDeque<>();
+        for (int i = 0; i < size; ++i) {
+            for (E el : a.get(i)) {
+                t.add(el);
+            }
         }
-        a[hash(value)].add(b);
+        size = size * 2 + 1;
+        a.clear();
+        a=new ArrayList<LinkedList<E>>(size);
+        for(int i=0;i<size;i++){
+            a.add(new LinkedList<E>());
+        }
+        while (!t.isEmpty()) {
+            add(t.getFirst());
+            t.pop();
+        }
+
     }
-    void search(V value){
-        System.out.println(a[hash(value)]);
+    void search(int value){
+        for(E t:a.get(hash(value))){
+            Polic c= (Polic) t;
+            if(c.inn == value){
+                System.out.println(c);
+            }
+        }
     }
-    void delete(V value){
-        a[hash(value)]=null;
+    void delete(int value){
+        for(E t:a.get(hash(value))){
+            Polic c= (Polic) t;
+            if(c.inn == value){
+                a.get(hash(value)).remove(t);
+            }
+        }
     }
     void printik(){
-        for(int i=0;i<1000;i++){
-            if(a[i]!=null) {
+        for(int i=0;i<size;i++){
+            if(!a.get(i).isEmpty()) {
                 System.out.println(i+" : ");
-                for (int j = 0; j < a[i].size(); j++) {
-                    System.out.println("\t"+a[i].get(j));
+                for (int j = 0; j < a.get(i).size(); j++) {
+                    System.out.println("\t"+a.get(i).get(j));
                 }
             }
         }
--- n10-practice11/src/spract_11/Main.java
+++ n10-practice11/src/spract_11/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        HashTable<Integer,Polic> ht=new HashTable<>();
-        ht.add(123,new Polic(123,"samsung","ivantsov"));
-        ht.add(1005,new Polic(1005,"samsung","ivantsov2"));
-        ht.add(5,new Polic(5,"samsung3","ivantsov3"));
+        HashTable<Integer,Polic> ht=new HashTable<>(10);
+        ht.add(new Polic(123,"samsung","ivantsov"));
+        ht.add(new Polic(1005,"samsung","ivantsov2"));
+        ht.add(new Polic(5,"samsung3","ivantsov3"));
+        ht.add(new Polic(2005,"samsung3","ivantsov4"));
+        ht.add(new Polic(3005,"samsung3","ivantsov5"));
         ht.printik();
+        System.out.println("\n");
         ht.search(1005);
+        System.out.println("\n");
         ht.delete(1005);
+        System.out.println("\n");
+        ht.printik();
     }
 }
